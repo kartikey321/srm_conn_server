e275e1102a31e888475ad58630f9802c8f0cb554
kartikey321 <kartikeymahawar1234@gmail.com>
2023-10-04 01:33:32 +0530

    bug fixes

--- bin/server.dart
+++ bin/server.dart
@@ -5,21 +5,19 @@ import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
 
-
-import '../routes/thread/index.dart' as thread_index;
-import '../routes/thread/[id].dart' as thread_$id;
-import '../routes/student/index.dart' as student_index;
-import '../routes/student/[id].dart' as student_$id;
-import '../routes/parent/index.dart' as parent_index;
-import '../routes/parent/[id].dart' as parent_$id;
-import '../routes/mail/index.dart' as mail_index;
-import '../routes/mail/[id].dart' as mail_$id;
+import '../routes/_middleware.dart' as middleware;
+import '../routes/faculty/details/[id].dart' as faculty_details_$id;
+import '../routes/faculty/details/index.dart' as faculty_details_index;
 import '../routes/faculty/index.dart' as faculty_index;
 import '../routes/faculty/updateclass/index.dart' as faculty_updateclass_index;
-import '../routes/faculty/details/index.dart' as faculty_details_index;
-import '../routes/faculty/details/[id].dart' as faculty_details_$id;
-
-import '../routes/_middleware.dart' as middleware;
+import '../routes/mail/[id].dart' as mail_$id;
+import '../routes/mail/index.dart' as mail_index;
+import '../routes/parent/[id].dart' as parent_$id;
+import '../routes/parent/index.dart' as parent_index;
+import '../routes/student/[id].dart' as student_$id;
+import '../routes/student/index.dart' as student_index;
+import '../routes/thread/[id].dart' as thread_$id;
+import '../routes/thread/index.dart' as thread_index;
 
 void main() async {
   final address = InternetAddress.anyIPv6;
@@ -30,69 +28,153 @@ void main() async {
 Future<HttpServer> createServer(InternetAddress address, int port) async {
   final handler = Cascade().add(buildRootHandler()).handler;
   final server = await serve(handler, address, port);
-  print('\x1B[92m✓\x1B[0m Running on http://${server.address.host}:${server.port}');
+  print(
+      '\x1B[92m✓\x1B[0m Running on http://${server.address.host}:${server.port}');
   return server;
 }
 
 Handler buildRootHandler() {
   final pipeline = const Pipeline().addMiddleware(middleware.middleware);
   final router = Router()
-    ..mount('/faculty/details', (context) => buildFacultyDetailsHandler()(context))
-    ..mount('/faculty/updateclass', (context) => buildFacultyUpdateclassHandler()(context))
-    ..mount('/faculty', (context) => buildFacultyHandler()(context))
-    ..mount('/mail', (context) => buildMailHandler()(context))
-    ..mount('/parent', (context) => buildParentHandler()(context))
-    ..mount('/student', (context) => buildStudentHandler()(context))
-    ..mount('/thread', (context) => buildThreadHandler()(context));
+    ..mount('/faculty/details',
+        (RequestContext context) => buildFacultyDetailsHandler()(context))
+    ..mount('/faculty/updateclass',
+        (RequestContext context) => buildFacultyUpdateclassHandler()(context))
+    ..mount(
+        '/faculty', (RequestContext context) => buildFacultyHandler()(context))
+    ..mount('/mail', (RequestContext context) => buildMailHandler()(context))
+    ..mount(
+        '/parent', (RequestContext context) => buildParentHandler()(context))
+    ..mount(
+        '/student', (RequestContext context) => buildStudentHandler()(context))
+    ..mount(
+        '/thread', (RequestContext context) => buildThreadHandler()(context));
   return pipeline.addHandler(router);
 }
 
 Handler buildFacultyDetailsHandler() {
   final pipeline = const Pipeline();
   final router = Router()
-    ..all('/', (context) => faculty_details_index.onRequest(context,))..all('/<id>', (context,id,) => faculty_details_$id.onRequest(context,id,));
+    ..all(
+        '/',
+        (RequestContext context) => faculty_details_index.onRequest(
+              context,
+            ))
+    ..all(
+        '/<id>',
+        (
+          RequestContext context,
+          String id,
+        ) =>
+            faculty_details_$id.onRequest(
+              context,
+              id,
+            ));
   return pipeline.addHandler(router);
 }
 
 Handler buildFacultyUpdateclassHandler() {
   final pipeline = const Pipeline();
   final router = Router()
-    ..all('/', (context) => faculty_updateclass_index.onRequest(context,));
+    ..all(
+        '/',
+        (RequestContext context) => faculty_updateclass_index.onRequest(
+              context,
+            ));
   return pipeline.addHandler(router);
 }
 
 Handler buildFacultyHandler() {
   final pipeline = const Pipeline();
   final router = Router()
-    ..all('/', (context) => faculty_index.onRequest(context,));
+    ..all(
+        '/',
+        (RequestContext context) => faculty_index.onRequest(
+              context,
+            ));
   return pipeline.addHandler(router);
 }
 
 Handler buildMailHandler() {
   final pipeline = const Pipeline();
   final router = Router()
-    ..all('/', (context) => mail_index.onRequest(context,))..all('/<id>', (context,id,) => mail_$id.onRequest(context,id,));
+    ..all(
+        '/',
+        (RequestContext context) => mail_index.onRequest(
+              context,
+            ))
+    ..all(
+        '/<id>',
+        (
+          RequestContext context,
+          String id,
+        ) =>
+            mail_$id.onRequest(
+              context,
+              id,
+            ));
   return pipeline.addHandler(router);
 }
 
 Handler buildParentHandler() {
   final pipeline = const Pipeline();
   final router = Router()
-    ..all('/', (context) => parent_index.onRequest(context,))..all('/<id>', (context,id,) => parent_$id.onRequest(context,id,));
+    ..all(
+        '/',
+        (RequestContext context) => parent_index.onRequest(
+              context,
+            ))
+    ..all(
+        '/<id>',
+        (
+          RequestContext context,
+          String id,
+        ) =>
+            parent_$id.onRequest(
+              context,
+              id,
+            ));
   return pipeline.addHandler(router);
 }
 
 Handler buildStudentHandler() {
   final pipeline = const Pipeline();
   final router = Router()
-    ..all('/', (context) => student_index.onRequest(context,))..all('/<id>', (context,id,) => student_$id.onRequest(context,id,));
+    ..all(
+        '/',
+        (RequestContext context) => student_index.onRequest(
+              context,
+            ))
+    ..all(
+        '/<id>',
+        (
+          RequestContext context,
+          String id,
+        ) =>
+            student_$id.onRequest(
+              context,
+              id,
+            ));
   return pipeline.addHandler(router);
 }
 
 Handler buildThreadHandler() {
   final pipeline = const Pipeline();
   final router = Router()
-    ..all('/', (context) => thread_index.onRequest(context,))..all('/<id>', (context,id,) => thread_$id.onRequest(context,id,));
+    ..all(
+        '/',
+        (RequestContext context) => thread_index.onRequest(
+              context,
+            ))
+    ..all(
+        '/<id>',
+        (
+          RequestContext context,
+          String id,
+        ) =>
+            thread_$id.onRequest(
+              context,
+              id,
+            ));
   return pipeline.addHandler(router);
 }
-
